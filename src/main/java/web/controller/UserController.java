74285bb22e6c2a19b121cef8fe694bbde6f3dc8f
Nikita <worldofstyks@gmail.com>
2024-12-10 17:48:47 +0300

    made basic view

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -1,4 +1,25 @@
 package web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import web.service.UserService;
+
+@Controller
+@RequestMapping("/users")
 public class UserController {
+
+    UserService userService;
+
+    private UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    private String getUsers(Model model) {
+        model.addAttribute("users", userService.getUsers());
+        return "users";
+    }
+
 }
