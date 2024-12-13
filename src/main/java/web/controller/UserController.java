d7576781f8bec8ee6bdd32cf647c1fc55e85707c
Nikita <worldofstyks@gmail.com>
2024-12-13 04:34:57 +0300

    added add user view

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -3,7 +3,9 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import web.model.User;
 import web.service.UserService;
 
 @Controller
@@ -16,10 +18,22 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/")
-    private String getUsers(Model model) {
+    @GetMapping("/list")
+    public String getUsers(Model model) {
         model.addAttribute("users", userService.getUsers());
-        return "users";
+        return "list";
+    }
+
+    @GetMapping("/add")
+    public String addUserForm(Model model) {
+        model.addAttribute("user", new User());
+        return "add";
+    }
+
+    @PostMapping("/add")
+    public String addUser(Model model) {
+
+        return "redirect:list";
     }
 
 }
